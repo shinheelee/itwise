8af0b3c2ef4f8db8efd047842d333f5f789109c4
leeshinhee <leeshinhee@DESKTOP-INSAPG0>
2017-10-31 15:15:22 +0900

    project

---
+++ src/main/java/kr/co/itwise/pilot/Pilot2Application.java
@@ -0,0 +1,12 @@
+package kr.co.itwise.pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pilot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pilot2Application.class, args);
+	}
+}
---
+++ src/main/java/kr/co/itwise/pilot/web/HelloWorldController.java
@@ -0,0 +1,14 @@
+package kr.co.itwise.pilot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public String getHelloWolrd(Model model) {
+		System.out.println("HERE!!!!!!!!!!!!!!!");
+		return "hello";
+	}
+}
